4587e96cf1daacb1fa4606403a1204d1947f20f8
Junchi-Liang <junchi_liang@163.com>
2016-10-02 10:12:46 -0400

    remove virtual from subclass

--- uniform_cost_search.h
+++ uniform_cost_search.h
@@ -9,7 +9,7 @@ namespace solver_ns
 	{
 		protected:
 			// return the f-value for the postion (cur_col, cur_row) while the goal is the (goal_col, goal_row) and the input map is map_input
-			virtual double f(double g, int cur_col, int cur_row, int goal_col, int goal_row, const map_maker_ns::grid_map &map_input)
+			double f(double g, int cur_col, int cur_row, int goal_col, int goal_row, const map_maker_ns::grid_map &map_input)
 			{
 				return g;
 			}
--- w_a_star.h
+++ w_a_star.h
@@ -11,7 +11,7 @@ namespace solver_ns
 			double w;
 
 			// return the f-value for the postion (cur_col, cur_row) while the goal is the (goal_col, goal_row) and the input map is map_input
-                        virtual double f(double g, int cur_col, int cur_row, int goal_col, int goal_row, const map_maker_ns::grid_map &map_input)
+                        double f(double g, int cur_col, int cur_row, int goal_col, int goal_row, const map_maker_ns::grid_map &map_input)
 			{
 				return g + w * heuristic->h(cur_col, cur_row, goal_col, goal_row, map_input);
 			}
